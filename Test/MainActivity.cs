d95ea0f2a219e2c65615069a2816c6f75998c070
btexterd <007sobaka@gmail.com>
2015-12-06 04:27:10 +0200

    Initial check-in of module Test

---
+++ Test/MainActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Test
+{
+	[Activity (Label = "Random Generator", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			WebSocketHelper wSH = new WebSocketHelper("ws://192.168.137.1:8080/WebApplication1/test");
+
+			SetContentView (Resource.Layout.Main);
+
+			Button button = FindViewById<Button> (Resource.Id.myButton);
+			button.Click += delegate {
+				button.Text = wSH.Send("");
+			};
+		}
+	}
+}
+
+
