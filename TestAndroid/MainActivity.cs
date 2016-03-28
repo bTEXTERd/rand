2794c4923507760b44cb256a7c739eca787803fc
btexterd <007sobaka@gmail.com>
2016-03-28 15:42:04 +0300

    First commit

---
+++ TestAndroid/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using MikePhil.Charting.Charts;
+
+namespace TestAndroid
+{
+    [Activity(Label = "TestAndroid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        int count = 1;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            WebSocketHelper wSH = new WebSocketHelper("wss://websockets9127.azurewebsites.net:443/ws.ashx?name=Android");
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+
+            LineChart chart = new LineChart(Application.Context);
+            LinearLayout rl = FindViewById<LinearLayout>(Resource.Id.LinearLayout);
+            rl.AddView(chart);
+
+            Button button = FindViewById<Button>(Resource.Id.MyButton);
+
+            button.Click += delegate { button.Text = wSH.Send("ASS"); };
+        }
+    }
+}
+
---
+++ TestAndroid/WebSocketHelper.cs
@@ -0,0 +1,35 @@
+using System;
+
+using System.Threading;
+using WebSocket4Net;
+
+namespace TestAndroid
+{
+    public class WebSocketHelper
+    {
+        private WebSocket webSocket;
+        private string _incomingMessage;
+        private AutoResetEvent _messageReceivedEvent = new AutoResetEvent(false);
+
+        public WebSocketHelper(string url)
+        {
+            webSocket = new WebSocket(url);
+            webSocket.MessageReceived += new EventHandler<MessageReceivedEventArgs>(webSocket_MessageReceived);
+            webSocket.Open();
+        }
+
+        public string Send(string message)
+        {
+            webSocket.Send(message);
+            this._messageReceivedEvent.WaitOne();
+            return this._incomingMessage;
+        }
+
+        private void webSocket_MessageReceived(object sender, MessageReceivedEventArgs e)
+        {
+            this._incomingMessage = e.Message;
+            this._messageReceivedEvent.Set();
+        }
+    }
+}
+
